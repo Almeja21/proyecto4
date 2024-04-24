388e30b468b7ab05114c7015fa1c96b03b3706b6
Almeja21 <151696313+Almeja21@users.noreply.github.com>
2024-04-23 18:15:54 -0600

    primercommit

---
+++ controllers/controllers.js
@@ -0,0 +1,93 @@
+let reservas = []
+
+const getReservas = async(req,res) =>{
+res.json({
+    msg: "Todas las reservas de la base de datos",
+    data: reservas
+})
+}
+
+const getReservasId= async(req,res) =>{
+    const orderID = parseInt(req.params.id)
+    const order = reservas.find((o) => o.id === orderID)
+    if(!order){
+        return res.status(404).json({ msg: "No se encuentra en la base de datos"})
+
+    }
+
+    res.json({
+        msg: "Reserva localizada",
+        data: order
+    })
+}
+
+const postReserva = async(req,res) =>{
+    const newReserva = req.body
+    newReserva.id = reservas.length + 1
+
+    reservas.push(newReserva)
+
+    res.status(200).json({
+        msg: "Reserva enviada",
+        data: reservas
+    })
+}
+
+const putReserva = async(req,res) =>{
+    const orderID = parseInt(req.params.id)
+    console.log(orderID)
+    const reservaIndex = reservas.findIndex((o) => o.id === orderID)
+    console.log(reservaIndex)
+    if(reservaIndex === -1){
+        return res.status(404).json({
+            msg: "Elemento no encontrado en la base, no es posible actualizar"
+        })
+    }
+
+    reservaIndex[orderID] = {...reservas[orderID], ...req.body}
+    
+    res.json({
+        msg: "Reserva actualizada",
+        data: reservas[reservaIndex]
+    })
+
+}
+
+const orderId = parseInt(req.params.id)
+const orderIndex = reservas.findIndex((o) => o.id === orderId)
+
+if(orderIndex === -1){
+    return res.status(404).json(
+        {
+            msg: "Reserva no encontrada"
+        }
+    )
+}
+
+rreservas[orderIndex] = {... reservas[orderIndex], ...req.body}
+res.json({
+    msg: 'Reserva actualizada con exito',
+    data: reservas[orderIndex]
+})
+
+
+
+
+const deleteReserva = async(req,res)=>{
+    const orderID = parseInt(req.params.id)
+    const reservaIndex = reservas.findIndex((o) => o.id === orderID)
+
+    reservas.splice(reservaIndex, 1)
+    res.json({
+        msg: "Dato eliminado correctamente"
+    })
+
+}
+
+module.exports = {
+    getReservas,
+    getReservasId,
+    postReserva,
+    putReserva,
+    deleteReserva,
+}
